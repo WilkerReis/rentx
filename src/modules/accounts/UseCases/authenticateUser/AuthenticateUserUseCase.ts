28a7a64635a292d5fdcfc1fa1b278d7b8d6781f1
Wilker Reis <wilkerdosreis@hotmail.com>
2022-01-18 10:56:19 -0300

    Tratamento de excecoes inserido com biblio express-async-errors

--- src/modules/accounts/UseCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/UseCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
+import AppError from '../../../../errors/AppError';
 import IUsersRepository from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -27,13 +28,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or password incorrect!');
+      throw new AppError('Email or password incorrect!');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect!');
+      throw new AppError('Email or password incorrect!');
     }
 
     const token = sign({}, '521f998b592a54179cec101bd2342abb', {
--- src/modules/accounts/UseCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/createUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import { hash } from 'bcryptjs';
 import ICreateUserDTO from '../../dtos/ICreateUserDTO';
 import IUsersRepository from '../../repositories/IUsersRepository';
+import AppError from '../../../../errors/AppError';
 
 @injectable()
 class CreateUserUseCase {
@@ -13,13 +14,13 @@ class CreateUserUseCase {
   async execute({
     name, email, password, driver_license,
   }: ICreateUserDTO): Promise<void> {
-    const passwordHash = await hash(password, 8);
-
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error('User already exists!');
+      throw new AppError('User already exists!');
     }
+
+    const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
       name,
